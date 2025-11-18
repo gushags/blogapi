69ac6b71509210ce2419b4cc311b9afcd9cb763c
gushags <jeff@hagkull.com>
2025-11-18 09:29:32 -0800

    Config: change seed file to reflect actual blog post data

--- config/seed.js
+++ config/seed.js
@@ -35,9 +35,28 @@ for (let i = 0; i < 20; i++) {
 
 // Faker data for posts
 const posts = [];
+const sampleContent = `<p>Writing a blog post is a very personal thing. It takes quite a lot of words, too. This blog is all about lorem ipsum. And how ipsum we can lorem.</p>
+<h2>This is a heading.</h2>
+<p>Below the heading quo usque tandem abutere, Catilina, patientia nostra? Cras mattis iudicium purus sit amet fermentum. Ullamco laboris nisi ut aliquid ex ea commodi consequat. Magna pars studiorum, prodita quaerimus. Gallia est omnis divisa in partes tres, quarum.</p>
+<ul>
+<li>We can bullet a list</li>
+<li>And another one</li>
+<li>Quisque ut dolor gravida, placerat libero vel, euismod.</li>
+<li>Phasellus laoreet lorem vel dolor tempus vehicula.</li>
+</ul>
+<p>Or we can have numbers:</p>
+<ol>
+<li>This is the first</li>
+<li>The second</li>
+<li>The third</li>
+</ol>
+<h3><span style="color: #236fa1;">This is a smaller heading. That we made blue.</span></h3>
+<p><span style="color: #000000;">And if we want, we can have a blockquote.</span></p>
+<p style="padding-left: 40px;"><span style="color: #000000;">Like this one. Magna pars studiorum, prodita quaerimus. Curabitur blandit tempus ardua ridiculus sed magna. Phasellus laoreet lorem vel dolor tempus vehicula.</span></p>
+<p><span style="color: #000000;">That's about it. Hope you enjoyed this sample blog post.</span></p>`;
 for (let i = 0; i < 10; i++) {
   const title = faker.word.words({ count: { min: 4, max: 6 } });
-  const content = faker.lorem.paragraphs({ min: 10, max: 15 });
+  const content = sampleContent;
   const published = true;
   const authorId = Math.floor(Math.random() * 20) + 1;
   posts.push({ title, content, published, authorId });
